a9173ff9c47bf2321a6959c9616d00cb6d8e93be
Edward Muller <edwardam@interlix.com>
2016-03-08 09:31:04 -0800

    Update version

--- lib/databasedotcom/version.rb
+++ lib/databasedotcom/version.rb
@@ -1,5 +1,5 @@
 # <b>DEPRECATED:</b> Please use <tt>restforce</tt> (https://github.com/ejholmes/restforce) instead.
 warn "DEPRECATED: Please use restforce (https://github.com/ejholmes/restforce) instead."
 module Databasedotcom
-  VERSION = "1.3.4"
+  VERSION = "1.3.5"
 end
